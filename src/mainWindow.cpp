a4ba65e110f603a4d834bf8875bb0c43d0674ae9
RavenGoku <raven.goku@gmail.com>
2024-02-03 14:10:51 +0000

    refractor code and make easier to read for standard operators

--- src/mainWindow.cpp
+++ src/mainWindow.cpp
@@ -134,23 +134,33 @@ namespace Calculator
 	System::Void Calculator::mainWindow::number_Inputs(System::Object ^sender, System::EventArgs ^e)
 	{
 		number_in = safe_cast<Button ^>(sender);
-		if(is_equal_clicked && num2 != 0)
+
+		if(!is_MainWindow_Modified)
 		{
-			tb_smallWindow->Clear();
+			if(tb_MainCalcText->Text == "0")
+			{
+				tb_MainCalcText->Text = number_in->Text;
+				is_MainWindow_Modified = true;
+			}
+			else
+			{
+				tb_MainCalcText->Text = number_in->Text;
+				is_MainWindow_Modified = true;
+			}
 		}
-		if(tb_MainCalcText->Text == "0")
+		else if(is_MainWindow_Modified)
 		{
-			tb_MainCalcText->Text = number_in->Text;
-			is_MainWindow_Modified = true;
-		}
-		else if(!is_MainWindow_Modified || is_operator_clicked)
-		{
-			tb_MainCalcText->Text = number_in->Text;
-			is_MainWindow_Modified = true;
-		}
-		else
-		{
-			tb_MainCalcText->Text += number_in->Text;
+			if(is_equal_clicked)
+			{
+				tb_smallWindow->Clear();
+				tb_MainCalcText->Text = number_in->Text;
+				is_equal_clicked = false;
+				is_operator_clicked = false;
+			}
+			else
+			{
+				tb_MainCalcText->Text += number_in->Text;
+			}
 		}
 	}
 	//========================================End Number buttons=====================================================
@@ -159,53 +169,56 @@ namespace Calculator
 	System::Void Calculator::mainWindow::operation_Buttons(System::Object ^sender, System::EventArgs ^e)
 	{
 		btn_operator = safe_cast<Button ^>(sender);
-
-		if(!is_operator_clicked)
+		if(is_MainWindow_Modified)
 		{
-			oper_char = btn_operator->Text;
-			if(is_special_op_clicked)
+			if(!is_operator_clicked)
 			{
-				tb_smallWindow->Text += " " + oper_char + " ";
-			}
-			else
-			{
+				oper_char = btn_operator->Text;
 				num1 = double::Parse(tb_MainCalcText->Text);
 				tb_smallWindow->Text = num1.ToString() + " " + oper_char + " ";
 				is_MainWindow_Modified = false;
+				is_operator_clicked = true;
+				is_equal_clicked = false;
+			}
+
+			else if(is_operator_clicked)
+			{
+				if(is_equal_clicked)
+				{
+					oper_char = btn_operator->Text;
+					num1 = double::Parse(tb_MainCalcText->Text);
+					tb_smallWindow->Text = num1.ToString() + " " + oper_char + " ";
+					is_MainWindow_Modified = false;
+					is_operator_clicked = true;
+					is_equal_clicked = false;
+				}
+				else if(!is_equal_clicked)
+				{
+					num2 = double::Parse(tb_MainCalcText->Text);
+					num1 = Calc(num1, oper_char, num2);
+					oper_char = btn_operator->Text;
+					tb_smallWindow->Text = num1.ToString() + " " + oper_char + " ";
+					tb_MainCalcText->Text = num1.ToString();
+					is_MainWindow_Modified = false;
+				}
 			}
 		}
 		else if(!is_MainWindow_Modified)
 		{
-			if(!is_operator_clicked)
+			if(is_operator_clicked)
 			{
-				tb_smallWindow->Text += " " + oper_char + " ";
+				oper_char = btn_operator->Text;
+				tb_smallWindow->Text = num1.ToString() + " " + oper_char + " ";
 			}
-			else
+			else if(!is_operator_clicked)
 			{
-				if(is_special_op_clicked)
-				{
-					oper_char = btn_operator->Text;
-					tb_smallWindow->Text = special_oper_char + " " + oper_char + " ";
-				}
-				else
-				{
-					oper_char = btn_operator->Text;
-					tb_smallWindow->Text = num1.ToString() + " " + oper_char + " ";
-				}
+				oper_char = btn_operator->Text;
+				num1 = double::Parse(tb_MainCalcText->Text);
+				tb_smallWindow->Text = num1.ToString() + " " + oper_char + " ";
+				is_MainWindow_Modified = false;
+				is_operator_clicked = true;
 			}
 		}
-		else
-		{
-			num2 = double::Parse(tb_MainCalcText->Text);
-			num1 = Calc(num1, oper_char, num2);
-			oper_char = btn_operator->Text;
-			tb_smallWindow->Text = num1.ToString() + " " + oper_char + " ";
-			tb_MainCalcText->Text = num1.ToString();
-			is_MainWindow_Modified = false;
-		}
-
-		is_operator_clicked = true;
-		is_equal_clicked = false;
 	}
 
 	System::Void mainWindow::special_operation_Buttons(System::Object ^sender, System::EventArgs ^e)
@@ -313,25 +326,54 @@ namespace Calculator
 	{
 		if(is_MainWindow_Modified)
 		{
-			if(is_equal_clicked && num2 != 0)
+			if(is_equal_clicked)
 			{
-				num1 = Double::Parse(tb_MainCalcText->Text);
-				tb_smallWindow->Text = num1.ToString() + " " + oper_char + " " + num2.ToString() + " =";
-				num1 = Calc(num1, oper_char, num2);
-				tb_MainCalcText->Text = num1.ToString();
-			}
-			else if(!is_operator_clicked)
-			{
-				num1 = double::Parse(tb_MainCalcText->Text);
-				tb_smallWindow->Text = num1.ToString() + " =";
-				is_MainWindow_Modified = false;
-			}
-			else if(is_operator_clicked)
-			{
-				if(is_special_op_clicked)
+				if(num2 != 0)
+				{
+					num1 = Double::Parse(tb_MainCalcText->Text);
+					if(num2 < 0)
+					{
+						tb_smallWindow->Text = num1.ToString() + " " + oper_char + " (" + num2.ToString() + ") =";
+					}
+					else
+					{
+						tb_smallWindow->Text = num1.ToString() + " " + oper_char + " " + num2.ToString() + " =";
+					}
+					num1 = Calc(num1, oper_char, num2);
+					tb_MainCalcText->Text = num1.ToString();
+				}
+				else if(is_operator_clicked)
 				{
 					num2 = double::Parse(tb_MainCalcText->Text);
-					tb_smallWindow->Text = special_oper_char + " " + oper_char + " " + num2.ToString() + " =";
+					tb_smallWindow->Text = num1.ToString() + " " + oper_char + " " + num2.ToString() + " =";
+					num1 = Calc(num1, oper_char, num2);
+					if(!is_divide_by_zero)
+					{
+						tb_MainCalcText->Text = num1.ToString();
+					}
+					is_MainWindow_Modified = true;
+					is_operator_clicked = false;
+				}
+				else if(!is_operator_clicked)
+				{
+					num1 = double::Parse(tb_MainCalcText->Text);
+					tb_smallWindow->Text = num1.ToString() + " =";
+					is_MainWindow_Modified = false;
+				}
+			}
+			else if(!is_equal_clicked)
+			{
+				if(num1 != 0)
+				{
+					num2 = Double::Parse(tb_MainCalcText->Text);
+					if(num2 < 0)
+					{
+						tb_smallWindow->Text = num1.ToString() + " " + oper_char + " (" + num2.ToString() + ") =";
+					}
+					else
+					{
+						tb_smallWindow->Text = num1.ToString() + " " + oper_char + " " + num2.ToString() + " =";
+					}
 					num1 = Calc(num1, oper_char, num2);
 					tb_MainCalcText->Text = num1.ToString();
 				}
@@ -349,7 +391,7 @@ namespace Calculator
 				}
 			}
 		}
-		else
+		else if(!is_MainWindow_Modified)
 		{
 			if(is_operator_clicked)
 			{
@@ -360,10 +402,17 @@ namespace Calculator
 					num1 = Calc(num1, oper_char, num2);
 					tb_MainCalcText->Text = num1.ToString();
 				}
-				else
+				else if(!is_special_op_clicked)
 				{
 					num2 = double::Parse(tb_MainCalcText->Text);
-					tb_smallWindow->Text = num1.ToString() + " " + oper_char + " " + num2.ToString() + " =";
+					if(num2 < 0)
+					{
+						tb_smallWindow->Text = num1.ToString() + " " + oper_char + " (" + num2.ToString() + ") =";
+					}
+					else
+					{
+						tb_smallWindow->Text = num1.ToString() + " " + oper_char + " " + num2.ToString() + " =";
+					}
 					num1 = Calc(num1, oper_char, num2);
 					if(!is_divide_by_zero)
 					{
@@ -372,23 +421,10 @@ namespace Calculator
 					is_MainWindow_Modified = true;
 				}
 			}
-			else
+			else if(!is_operator_clicked)
 			{
-				if(is_special_op_clicked)
-				{
-					tb_smallWindow->Text += " =";
-				}
-				else
-				{
-					tb_smallWindow->Text = num1.ToString() + " =";
-				}
-
-				//tb_smallWindow->Text = num1.ToString() + " " + oper_char + " " + num2.ToString() + " =";
-				//num1 = Calc(num1, oper_char, num2);
-				//tb_MainCalcText->Text = num1.ToString();
 			}
 		}
-		is_special_op_clicked = false;
 		is_equal_clicked = true;
 		//============================== END Operator equality = =====================================================
 	}
